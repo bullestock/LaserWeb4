9cd06da9c53abe7f84aca26ab54579bfb79a3fa8
Owen Carter <owen.carter@gmail.com>
2021-02-25 17:47:27 +0100

    Improve connecting to active machine

--- src/components/com.js
+++ src/components/com.js
@@ -245,9 +245,19 @@ class Com extends React.Component {
         });
 
         socket.on('runningJob', function (data) {
-            CommandHistory.write('runningJob(' + data.length + ')', CommandHistory.WARN);
-            alert(data);
+            CommandHistory.write('Running Job!', CommandHistory.WARN);
+            // When connecting to a server that is already running a job, do NOT always pop up an alert with the Job data (Gcode) since a few MB from
+            //  a large job can hang the web browser as it loads and displays. Only Popup an alert if it is short.
+            if (data.length < 512) {
+                CommandHistory.write(data, CommandHistory.STD);
+                alert(data);
+            } else {
+                CommandHistory.write('Running code size: ' + data.length + ', Current progress unavailable', CommandHistory.STD);
+                alert('A Job is Currently Running.<br/>It is ' + data.length + ' bytes long, Current progress is not available.');
+            }
             //setGcode(data);
+            // Do not get running gcode here, there is a seperate call to lw.comm.server 'getRunningJob' that could be used for this purpose.
+            // The user should be alerted first, since large data packets from the server can floor the browser while it is recieving and digesting them.
         });
 
         socket.on('runStatus', function (status) {
